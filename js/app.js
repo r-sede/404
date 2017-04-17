3b1e3b4bb80c4299702db3145a3aa8a18e9e52fd
r-sede <r_sede@orange.fr>
2017-04-17 18:30:22 +0200

    nearly done :)

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log('hello');
+jQuery(document).ready(function(){
+  jQuery('#parallax .parallax-layer')
+  .parallax({
+    mouseport: jQuery('html')
+  },
+  {
+
+    //backgroundsky
+    xparallax: 0.2,
+    yparallax: 0.2
+  },{},{},{
+    yparallax: 0.6
+  },{
+    xparallax: false,
+    yparallax: false
+  });
+});
